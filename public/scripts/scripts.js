97ae8046c182a2659fa449c38484c8dd20bf7c84
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-06-27 11:45:34 -0300

    removed modal, since is not going to be used anymore

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,5 +1,4 @@
 const cards = document.querySelectorAll('.card')
-const modalOverlay = document.querySelector('.modal-overlay')
 
 for (let card of cards) {
     card.addEventListener("click", function () {
@@ -7,18 +6,6 @@ for (let card of cards) {
         const cardFood = card.querySelector('.food').innerHTML
         const cardChef = card.querySelector('.card__info p').innerHTML
 
-        modalOverlay.querySelector('img').src = `../assets/${cardId}.png`
-        modalOverlay.querySelector('.food').innerHTML = cardFood
-        modalOverlay.querySelector('.card__info p').innerHTML = cardChef
-
-        modalOverlay.classList.add('active')
+        
     })
 }
-
-modalOverlay.querySelector('.close-modal').addEventListener("click", function () {
-    modalOverlay.classList.remove('active')
-    modalOverlay.querySelector('.card__image-container img').src = ""
-    modalOverlay.querySelector('.food').innerHTML = null
-    modalOverlay.querySelector('.card__info p').innerHTML = null
-})
-
