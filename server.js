de5368c18992e882d43f528c2f0d6727e0e66e6b
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-06-27 11:56:44 -0300

    adjustments on server and views to render pages

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const data = require('./data')
 
 const server = express()
 
 server.set("view engine", "njk")
+
 server.use(express.static('public'))
 
 nunjucks.configure("views", {
-     express: server
-    })
+    express: server
+})
 
 server.get('/', (req, res) => {
-    res.render("home")
+    res.render("home", { item: data })
+})
+
+server.get('/about', (req, res) => {
+    res.render("about")
+})
+
+server.get('/recipes', (req, res) => {
+    res.render("recipes", { item: data })
 })
 
 server.listen(5000, () => {
