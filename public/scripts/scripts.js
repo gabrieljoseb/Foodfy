ba37f8d6f1311776efa376c52246bc12898adec0
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-06-28 22:59:43 -0300

    finished the backend of the application

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,4 +1,7 @@
 const cards = document.querySelectorAll('.card')
+const ingredientsButton = document.querySelector('.ingredients .main a')
+const preparationButton = document.querySelector('.preparation .main a')
+const informationButton = document.querySelector('.information .main a')
 
 for (let i = 0; i < cards.length; i++) {
     (
@@ -7,5 +10,30 @@ for (let i = 0; i < cards.length; i++) {
                 window.location.href = `/recipes/${index}`
             })
         }
-    ) (i)
+    )(i)
 }
+
+function changeButtonName(button) {
+    if (button.textContent == 'esconder')
+        button.textContent = 'mostrar'
+    else
+        button.textContent = 'esconder'
+}
+
+ingredientsButton.addEventListener('click', () => {
+    changeButtonName(ingredientsButton)
+
+    document.querySelector('#ingredients-text').classList.toggle('hide')
+})
+
+preparationButton.addEventListener('click', () => {
+    changeButtonName(preparationButton)
+
+    document.querySelector('#preparation-text').classList.toggle('hide')
+})
+
+informationButton.addEventListener('click', () => {
+    changeButtonName(informationButton)
+
+    document.querySelector('#information-text').classList.toggle('hide')
+})
