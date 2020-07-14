2589fbc6d9ddc1aa19789ac57c570ef06dda678d
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-07-14 18:27:43 -0300

    added routes to admin views and add 'adminRecipes' controller

---
+++ controllers/adminRecipes.js
@@ -0,0 +1,28 @@
+const data = require('../data')
+
+exports.index = (req, res) => {
+    return res.render("home")
+}
+
+exports.create = (req, res) => {
+    return res.render("home")
+}
+
+exports.show = (req, res) => {
+    return res.render("home")
+}
+
+exports.edit = (req, res) => {
+    return res.render("home")
+}
+
+exports.post = (req, res) => {
+    return res.render("home")
+}
+
+exports.put = (req, res) => {
+    return res.render("home")
+}
+exports.delete = (req, res) => {
+    return res.render("home")
+}
--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const routes = express.Router()
 const data = require('./data')
 const recipes = require('./controllers/recipes')
+const adminRecipes = require('./controllers/adminRecipes')
 
 routes.get('/', (req, res) => {
     return res.redirect('/index');
@@ -11,6 +12,15 @@ routes.get('/index', recipes.index)
 routes.get('/about', recipes.about)
 routes.get('/recipes', recipes.show)
 routes.get('/recipes/:index', recipes.recipe)
+
+
+routes.get("/admin/recipes", adminRecipes.index);
+routes.get("/admin/recipes/create", adminRecipes.create);
+routes.get("/admin/recipes/:id", adminRecipes.show);
+routes.get("/admin/recipes/:id/edit", adminRecipes.edit);
+routes.post("/admin/recipes", adminRecipes.post);
+routes.put("/admin/recipes", adminRecipes.put);
+routes.delete("/admin/recipes", adminRecipes.delete);
 
 
 module.exports = routes
